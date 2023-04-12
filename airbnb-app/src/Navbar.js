405f1a20d156f52a9bf0baa6516e0dd86f65d64c
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2023-04-12 16:22:47 +0300

    first commit

---
+++ airbnb-app/src/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Navbar() {
+    return (
+        <nav>
+            {/* <img src="../images/airbnb-logo.png" /> */}
+        </nav>
+    )
+}
+
+export default Navbar
